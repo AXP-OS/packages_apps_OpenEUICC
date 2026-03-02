dc56b1418f62b1ccf0b7f4e776e478cf739817aa
steadfasterX <steadfasterx@binbash.rocks>
2026-03-02 15:37:57 +0000

    Update buildSrc/src/main/kotlin/im/angry/openeuicc/build/Versioning.kt

--- buildSrc/src/main/kotlin/im/angry/openeuicc/build/Versioning.kt
+++ buildSrc/src/main/kotlin/im/angry/openeuicc/build/Versioning.kt
@@ -1,57 +1,33 @@
 package im.angry.openeuicc.build
 
-import com.android.build.gradle.internal.api.ApkVariantOutputImpl
-import com.android.build.gradle.internal.dsl.BaseAppModuleExtension
-import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.configure
-import java.io.ByteArrayOutputStream
 
-val Project.gitVersionCode: Int
-    get() =
-        try {
-            val stdout = ByteArrayOutputStream()
-            exec {
-                commandLine("git", "rev-list", "--first-parent", "--count", "HEAD")
-                standardOutput = stdout
-            }
-            stdout.toString("utf-8").trim('\n').toInt()
-        } catch (_: Exception) {
-            0
-        }
+object Versioning {
 
-fun Project.getGitVersionName(vararg args: String): String =
-    try {
-        val stdout = ByteArrayOutputStream()
-        exec {
-            commandLine("git", "describe", "--always", "--tags", "--dirty", *args)
-            standardOutput = stdout
+    private fun getRequiredProperty(project: Project, name: String): String {
+        val value = project.rootProject.findProperty(name)?.toString()
+        require(!value.isNullOrBlank()) {
+            "Required property '$name' not found in root gradle.properties"
         }
-        stdout.toString("utf-8").trim('\n').removePrefix("unpriv-")
-    } catch (_: Exception) {
-        "Unknown"
+        return value
     }
 
+    fun getVersionName(project: Project): String {
+        return getRequiredProperty(project, "versionName")
+    }
 
-class MyVersioningPlugin : Plugin<Project> {
-    override fun apply(target: Project) {
-        target.configure<BaseAppModuleExtension> {
-            defaultConfig {
-                versionCode = target.gitVersionCode
-                versionName = target.getGitVersionName()
-            }
+    fun getVersionCode(project: Project): Int {
+        return getRequiredProperty(project, "versionCode").toInt()
+    }
 
-            applicationVariants.all {
-                if (name == "debug") {
-                    outputs.forEach {
-                        with(it as ApkVariantOutputImpl) {
-                            versionCodeOverride = (System.currentTimeMillis() / 1000).toInt()
-                            // always keep the format: <tag>-<commits>-g<hash>[-dirty]
-                            versionNameOverride = target.getGitVersionName("--long")
-                        }
-                    }
-                }
-            }
-        }
+    fun applyVersioning(project: Project) {
+        project.extensions.extraProperties.set(
+            "versionName",
+            getVersionName(project)
+        )
+        project.extensions.extraProperties.set(
+            "versionCode",
+            getVersionCode(project)
+        )
     }
 }
